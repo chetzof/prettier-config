ae0caf90bc0066264ac07035a13957e3f028631e
Vlad Cos <vcosvic@gmail.com>
2023-06-07 02:48:28 +0300

    fix: renamed

--- .projenrc.ts
+++ .projenrc.ts
@@ -6,10 +6,8 @@ const project = new CustomTypescriptProject({
   name: "@chetzof/prettier-config",
   projenrcTs: true,
   releaseToNpm: true,
-  mutableBuild: true
-
-  // deps: [],                /* Runtime dependencies of this module. */
-  // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
-  // packageName: undefined,  /* The "name" in package.json. */
+  mutableBuild: true,
+  peerDeps: ['prettier'],
 });
+project.compileTask.reset(`cp src/index.js  ${project.libdir}/index.js`)
 project.synth();
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-export default {
+module.exports =  {
   singleQuote: true,
   semi: false,
   printWidth: 80,
