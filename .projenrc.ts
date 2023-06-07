63ab45ffee06cce67692d6d5864bacf30ecccccf
Vlad Cos <vcosvic@gmail.com>
2023-06-07 03:15:07 +0300

    fix: renamed

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,13 +1,16 @@
-import { CustomTypescriptProject } from "@chetzof/projen-base"
+import { CustomTypescriptProject } from '@chetzof/projen-base'
 
 const project = new CustomTypescriptProject({
-  defaultReleaseBranch: "main",
-  devDeps: ["@chetzof/projen-base"],
-  name: "@chetzof/prettier-config",
+  defaultReleaseBranch: 'main',
+  devDeps: ['@chetzof/projen-base'],
+  name: '@chetzof/prettier-config',
   projenrcTs: true,
   releaseToNpm: true,
   mutableBuild: true,
   peerDeps: ['prettier'],
-});
-project.compileTask.reset(`mkdir ${project.libdir} &&  cp src/index.js  ${project.libdir}/index.js`)
-project.synth();
+})
+project.compileTask.reset(
+  `mkdir ${project.libdir} &&  cp src/index.js  ${project.libdir}/index.js`,
+)
+project.package.addField('prettier', './src/index.js')
+project.synth()
