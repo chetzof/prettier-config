7f010470268633418a87f36f99915064996ab5bb
Vlad Cos <vcosvic@gmail.com>
2023-06-09 00:51:51 +0300

    WIP

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,21 +1,17 @@
-import { CustomTypescriptProject } from '@chetzof/projen-base'
+import { CustomTypescriptProject } from '@vladcos/projen-base'
 
 const project = new CustomTypescriptProject({
   defaultReleaseBranch: 'main',
-  devDeps: ['@chetzof/projen-base'],
-  name: '@chetzof/prettier-config',
+  devDeps: ['@vladcos/projen-base'],
+  name: '@vladcos/prettier-config',
   projenrcTs: true,
   releaseToNpm: true,
   mutableBuild: true,
   peerDeps: ['prettier'],
-  depsUpgradeOptions: {
-    exclude: ['@chetzof/projen-base', 'projen'],
-  },
 })
 project.compileTask.reset(
   `mkdir ${project.libdir} &&  cp src/index.js  ${project.libdir}/index.js`,
 )
 project.package.addField('prettier', './src/index.js')
-project.deps.removeDependency('@chetzof/prettier-config')
 
 project.synth()
