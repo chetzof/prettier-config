ec7cd74aab0a67366439a82ab5276c2547989995
Vlad Cos <vcosvic@gmail.com>
2024-03-01 00:17:33 +0200

    chrore: updated repo url

--- .projenrc.ts
+++ .projenrc.ts
@@ -9,7 +9,7 @@ const project = new TypeScriptProject({
   mutableBuild: true,
   packemon: false,
   projenDevDependency: false,
-  repository: 'https://github.com/vladcosorg/eslint-config',
+  repository: 'https://github.com/vladcosorg/prettier-config',
 })
 project.compileTask.reset(
   `mkdir ${project.libdir} &&  cp src/index.js  ${project.libdir}/index.js`,
